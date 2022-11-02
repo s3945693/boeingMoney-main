f01e8b8e0dc85afa5d9b920de34058531c8a8d41
Frederick Hadi <gangstapotato36@gmail.com>
2022-11-03 10:02:14 +1100

    working indexing engine

--- process_json.py
+++ process_json.py
@@ -7,7 +7,7 @@ import glob
 
 trial = 'AutoClave/*'
 fileList = glob.glob(trial)
-print('This was fileList:', fileList)
+#print('This was fileList:', fileList)
 part_info_df = pd.DataFrame()
 run_details_df = pd.DataFrame()
 first_run = True
@@ -21,19 +21,18 @@ for file in fileList:
     #should do data[PartInformation][PartNumber]
     part_info_df_temp['LoadNumber'] = data['RunDetails']['LoadNumber']
 
-    run_details_df_temp = data['RunDetails']
-
-
+    run_details_df_temp = pd.json_normalize(data['RunDetails'])
 
     if first_run == True:
         part_info_df = part_info_df_temp.copy()
         run_details_df = run_details_df_temp.copy()
-        first_run == False
+        first_run = False
     else:
-        part_info_df.append(part_info_df_temp, ignore_index=True)
-        run_details_df.append(run_details_df_temp, ignore_index=True)
+        part_info_df = part_info_df.append(part_info_df_temp, ignore_index=True)
+        run_details_df = run_details_df.append(run_details_df_temp, ignore_index=True)
 
 
 #temp = pd.concat(df, ignore_index=True)
 print(run_details_df)
+
 
