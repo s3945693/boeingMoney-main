26cdf23eca073d75ba2d87cb4c56252439fdbfcb
s3945693 <s3945693@student.rmit.edu.au>
2022-11-02 13:41:06 +1100

    website done

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
 from flask import Flask
 from datetime import datetime
 import re
+from flask import render_template
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template("home.html")
 
 
 @app.route("/hello/<name>")
-def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+def hello_there(name = None):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
 
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return content
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
