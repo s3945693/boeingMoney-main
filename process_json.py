a50a71e2047f4d0f05feb8b4cb4698de4a9820cd
s3945693 <s3945693@student.rmit.edu.au>
2022-11-02 22:27:38 +1100

    scripted df creation

--- process_json.py
+++ process_json.py
@@ -1,14 +1,27 @@
 import pandas as pd
 import numpy as np
 import json
+import os
+import glob
+#https://stackoverflow.com/questions/57067551/how-to-read-multiple-json-files-into-pandas-dataframe
+
+trial = 'json/*'
 
 
-f = open("C:/Users/frede/Downloads/AutoClave5 JSON Files/AC2-07337-anon.json")
-data = json.loads(f.read())
-part_info_df = pd.json_normalize(data['PartInformation'], meta=data['RunDetails'])
-part_info_df['load_no'] = data['PartInformation']
-print(part_info_df)
+fileList = glob.glob(trial)
+print('This was fileList:',fileList)
+df = []
 
-#test = pd.json_normalize("C:/Users/frede/Downloads/AutoClave5 JSON Files/AC2-07337-anon.json")
-#test.head(10)
-#print('what')
+for file in fileList:
+    f = open(file)
+    data = json.loads(f.read())
+
+    part_info_df = pd.json_normalize(data['PartInformation'], meta=data['RunDetails'])
+    part_info_df['load_no'] = data['PartInformation']
+
+    df.append(part_info_df)
+
+
+temp = pd.concat(df, ignore_index=True)
+print(temp)
+
